0968af007d690ff71c0137e3313662ac58283854
Dustin Ashley <dashley@virtualhold.com>
2015-01-21 13:15:21 -0500

    Created broken rspec stuff... pushing as a starting point.

--- lib/getLogs.rb
+++ lib/getLogs.rb
@@ -1,29 +1,36 @@
 require_relative "helpers"
 
-Reg_path = "SOFTWARE\\Wow6432Node\\Virtual Hold"
-Log_key = "MainLogFilePath"
+# From Console
 
-log_path = RegistryHelpers.read_value(Reg_path, Log_key)
-puts "Your log file path is: #{log_path}\n"
+v1 = ARGV[0]
 
-main_date_folders = Dir.entries(log_path+"\\Main")
-main_date_folders.delete_at(0)
-main_date_folders.delete_at(0)
-puts "\n"
-puts "Main Output Folders:\n"
-puts "\t"+main_date_folders[0].to_s
+puts v1
 
-main_files = []
-
-puts "\n"
-puts "Main Output Files:\n"
-main_date_folders.each do |folder|
-  temp_files = Dir.entries(log_path+"\\Main\\"+folder.to_s)
-  temp_files.delete_at(0)
-  temp_files.delete_at(0)
-  main_files += temp_files
-end
-
-main_files.each do |filename|
-    puts "\t"+filename.to_s
-end
+#
+# Reg_path = "SOFTWARE\\Wow6432Node\\Virtual Hold"
+# Log_key = "MainLogFilePath"
+#
+# log_path = RegistryHelpers.read_value(Reg_path, Log_key)
+# puts "Your log file path is: #{log_path}\n"
+#
+# main_date_folders = Dir.entries(log_path+"\\Main")
+# main_date_folders.delete_at(0)
+# main_date_folders.delete_at(0)
+# puts "\n"
+# puts "Main Output Folders:\n"
+# puts "\t"+main_date_folders[0].to_s
+#
+# main_files = []
+#
+# puts "\n"
+# puts "Main Output Files:\n"
+# main_date_folders.each do |folder|
+#   temp_files = Dir.entries(log_path+"\\Main\\"+folder.to_s)
+#   temp_files.delete_at(0)
+#   temp_files.delete_at(0)
+#   main_files += temp_files
+# end
+#
+# main_files.each do |filename|
+#     puts "\t"+filename.to_s
+# end
--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,5 +1,9 @@
-describe "a sample" do
-  it "should pass" do
-    # true
+require_relative "MainOutputLog"
+
+
+describe "object exists" do
+  it "returns an object" do
+    mainLog = MainOutputLogFile.new
+    mainLog.should_not == nil
   end
 end
