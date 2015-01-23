c6a28d1f6c740ccd36aec14059233f41a18b9791
Justin Ranta <jranta@virtualhold.com>
2015-01-23 13:39:49 -0500

    [#86460670] fixed backslashes

--- spec/version_parser_spec.rb
+++ spec/version_parser_spec.rb
@@ -4,7 +4,7 @@ require 'version_parser'
 describe 'VersionParser' do
 
   it 'should return correct version' do
-    filename = 'TestLogs\VHLogs\Logs\2015_01_21\Main\09_54_15\MainOutputLog_00001.txt'
+    filename = 'TestLogs/VHLogs/Logs/2015_01_21/Main/09_54_15/MainOutputLog_00001.txt'
     parser = VersionParser.new
     parser.get_version_from_file(filename).should == 'Version 7, 6, 10, 2943'
   end
