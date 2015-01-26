0aded1e9c99bc63a69669bc87a1e6454539c4d84
Marco Adkins <marcoadkins@Marcos-MacBook-Pro.local>
2015-01-26 14:49:13 -0500

    removed uneeded comments

--- spec/logSearch_spec.rb
+++ spec/logSearch_spec.rb
@@ -4,7 +4,7 @@ require_relative '../lib/logSearch'
 
 describe 'Log search should return correct search results' do
 
-  it 'After searching test.txt, output.txt should contain the correct number of lines' do
+  it 'After searching test.txt, it should return an array containing the correct number of lines' do
     log = LogSearch.new
     expect(log.search('spec/test.txt' , 'Utilities').length).to eq(11)
   end
