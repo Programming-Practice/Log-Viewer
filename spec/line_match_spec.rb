d9d966b50d5fd6cb5d8b18f445fc809837752b75
Brian H. Ward <glyphrider@gmail.com>
2015-02-10 17:13:34 -0500

    [#87127240] refactored the rspec tests to remove some duplication

--- spec/line_match_spec.rb
+++ spec/line_match_spec.rb
@@ -2,25 +2,22 @@ require 'spec_helper'
 
 describe LineMatch do
 
+  let(:subject) { LineMatch.new(4,'This should be returned') }
+
   it 'should return the line' do
-    line_num = 4
-    line = 'This should be returned'
-    results = LineMatch.new(line_num,line)
-    expect(results.line).to eq('This should be returned')
+    expect(subject.line).to eq('This should be returned')
   end
 
   it 'should return the line number' do
-    line_num = 4
-    line = 'This should be returned'
-    results = LineMatch.new(line_num,line)
-    expect(results.line_num).to eq(4)
+    expect(subject.line_num).to eq(4)
   end
 
   it 'should return a string of line number and the line' do
-    line_num = 4
-    line = 'This should be returned'
-    results = LineMatch.new(line_num,line)
-    expect(results.to_s).to eq('4 This should be returned')
+    expect(subject.to_s).to eq('4 This should be returned')
+  end
+
+  it 'should convert to a string automatically' do
+    expect("#{subject}").to eq('4 This should be returned')
   end
 
 end
