c925d92fb61348bb7ad02a4d39ce8cce5ec41385
Tim Gardiner <tgar1989@gmail.com>
2015-02-11 13:07:57 -0500

    86633126: added the function and some generic pseudo code for our plans on parsing

--- lib/log_search.rb
+++ lib/log_search.rb
@@ -19,4 +19,27 @@ class LogSearch
     line.match(stringToFind)
   end
 
+  def number_of_call_IDs(log)
+    line_num = 0
+    File.read(log).each_line {|line|
+      line_num += 1
+      if (line_matches(line,"Interaction Created") and line_matches(line,"New Call") and line_matches(line,"Start"))
+        {
+          # split string => keep everything after "Start|"
+          # split string again => keep eveyrthing before "|"
+          # Add to call ID list (if not already in list)
+        }
+      end
+    }
+  end
+
 end
+
+
+
+2015-01-21 09:54:38.531|INFO|CVHTCall::OnChange|Start|V0002000000000221012015095438531|NULL|New Call| [Interaction Created]|1aa4
+2015-01-21 09:54:38.531|Info|CCall::Change|CHANGE_DEVICES|No Device found|V0002000000000221012015095438531|State|New Call|2015-01-21 09:54:38.531|1aa4
+2015-01-21 09:54:38.532|Info|CCall::Change|CHANGE_DEVICES|No Device found|V0002000000000221012015095438531|State|Interaction Created|2015-01-21 09:54:38.531|1aa4
+2015-01-21 09:54:38.532|CCallStateTimerManager::StartCallStateTimer|CreateTimerQueueTimer succeeded|HANDLE = 9271992|CallId = V0002000000000221012015095438531|CALLINFO Value = Interaction Expired|Reason = Interaction Expired|Timeout = 900000|1aa4
+2015-01-21 09:54:38.532|INFO|CVHTCall::OnChange|State Change|V0002000000000221012015095438531| NULL -> (New Call) -> Interaction Created|1aa4
+2015-01-21 09:54:38.532|INFO|VHTCall|OnChange|End|V0002000000000221012015095438531|Interaction Created|New Call| [Interaction Created]|1aa4
