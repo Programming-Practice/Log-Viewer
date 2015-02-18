8f15cf86a95592319eca4b0f71529d53f3fa3e57
Marco Adkins <madkins@virtualhold.com>
2015-02-18 12:04:24 -0500

    Added 'or' and 'and' functionality to search function and created tests around new functionality

--- lib/log_search.rb
+++ lib/log_search.rb
@@ -1,19 +1,56 @@
 class LogSearch
 
-  def search(log, stringToFind)
+
+  def search(log, stringToFind, operator)
 
     raise IOError,"The log \"#{log}\" does not exist."  unless File.exist? log
 
     results = Array.new
     line_num = 0
-
     File.read(log).each_line {|line|
       line_num += 1
-      results << LineMatch.new(line_num,line) if line_matches(line,stringToFind)
+      results << LineMatch.new(line_num,line) if eval(build_code_string(operator, stringToFind))
     }
-    return results
+
+    results
 
   end
+
+  def build_code_string(operator, stringToFind)
+    code = ' '
+    if operator == 'or'
+      stringToFind.each_index { |index|
+        if index == 0
+          code << "line_matches(line, stringToFind[#{index}])"
+        else
+          code << " || line_matches(line, stringToFind[#{index}])"
+        end
+      }
+    end
+
+    if operator == 'and'
+      stringToFind.each_index { |index|
+        if index == 0
+          code << "line_matches(line, stringToFind[#{index}])"
+        else
+          code << " && line_matches(line, stringToFind[#{index}])"
+        end
+      }
+    end
+
+    if operator == nil
+      stringToFind.each_index { |index|
+        if index > 0
+          raise IOError,'An operator was not passed for multiple keywords'
+        end
+        code << "line_matches(line, stringToFind[#{index}])"
+      }
+    end
+
+    return code
+  end
+
+
 
   def line_matches(line, stringToFind)
     line.match(stringToFind)
