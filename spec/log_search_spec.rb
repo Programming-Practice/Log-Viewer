85a728f2412407ec63ffb67959c263fa25bbb36b
Dustin Ashley <dashley@virtualhold.com>
2015-02-18 12:52:59 -0500

    Added another rspec for testing that 0 is returned if passed a file with no logs.

--- spec/log_search_spec.rb
+++ spec/log_search_spec.rb
@@ -16,6 +16,11 @@ describe LogSearch do
     end
   end
 
+  it 'should return 0 if passed a file with no logs in it' do
+    expect(subject.number_of_call_ids('TestLogs/empty_folder/readme.txt')).to equal(0)
+  end
+
+
   it 'should return the correct number of call IDs' do
     expect(subject.number_of_call_ids('TestLogs/test.txt')).to equal(4)
   end
