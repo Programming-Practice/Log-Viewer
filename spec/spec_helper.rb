e0cbd778e658f6ba95935c8e83952799f13b7fc9
Jerry D'Antonio <stumpjumper@gmail.com>
2015-02-23 11:42:12 -0500

    Updated gem config to be more conventional and fix broken executables.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,4 @@
 require 'simplecov'
 SimpleCov.start
 
-$:.unshift File.dirname(__FILE__)+'../lib'
-
-require 'version_parser'
-require 'log_file_finder'
-require 'log_search'
-require 'insensitive_log_search'
-require 'line_match'
-require 'search_terms/keyword'
-require 'search_terms/and'
-require 'search_terms/or'
-require 'search_terms/any'
+require 'pp_log_viewer'
